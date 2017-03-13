251647fdbdecc189143e309b9c7fe509ea13e4e5
Jack Tattersall <jacktattersall1@gmail.com>
2017-03-13 12:46:23 +0000

    created model for list Items and associated migrations

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+from lists.models import Item
 import re
 
 
@@ -47,11 +48,31 @@ class HomePageTest(TestCase):
 
         self.assertIn('A new list item', expected_html)
 
+class ItemModelTest(TestCase):
 
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second list item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(second_saved_item.text, 'The second list item')
+
+# <editor-fold desc = "Helper Functions" >
 # Function that removes a line from a string if the line contains 'rem' and removes empty lines also
 def remove(rem, my_string):
     my_string = re.sub(".*" + rem + ".*\n?", "", my_string)
     my_string = os.linesep.join([s for s in my_string.splitlines() if s.strip()])
     return my_string
+# </editor-fold>
 
 
