d90a16caed33d0198acfb0d2a0bb834748014547
Jack Tattersall <jacktattersall1@gmail.com>
2017-03-13 09:50:17 +0000

    Functional test now checks we  can inut a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -12,23 +13,38 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+
         # Jack has heard about a new cool to-do list app
         # He goes to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # He Notices the page title and header mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types 'buy new water bottle and cage' into a text box
+        inputbox.send_keys('buy new water bottle and cage')
 
         # When he hits enter the page updates and now the page lists
         # "1: buy new water bottle and cage" as an item in the to-do list
+        inputbox.send_keys(Keys.Enter)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: buy new water bottle and cage' for row in rows)
+        )
 
         # There is still a text box inviting him to enter another item
         # he adds 'clean bicycle'
+        self.fail("Finish the tests")
 
         # The page updates again and he can now see both items
 
